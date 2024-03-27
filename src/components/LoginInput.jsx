b27b8805f87c0fcd2dfba25532910669a68c3928
Rizki-Malik <rimali.qwerty@gmail.com>
2024-03-27 10:34:41 +0700

    feat: add parameter login onClick in button LoginInput

--- src/components/LoginInput.jsx
+++ src/components/LoginInput.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import useInput from '../hooks/useInput';
+import useInput from '../hooks/useInput'; // Import useInput hook
+
 export default function LoginInput({ login }) {
+  // Destructure input value and input change handler from useInput hook
   const [email, setEmail] = useInput('');
   const [password, setPassword] = useInput('');
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    login({ email, password });
+  };
+
   return (
-    <form className="login-form">
+    <form className="login-form" onSubmit={handleSubmit}>
       <div className="w-72 inline-block justify-center">
         <div className="login-field">
           <input
@@ -17,10 +24,9 @@ export default function LoginInput({ login }) {
             value={email}
             onChange={setEmail}
             autoFocus
-            placeholder=" " />
-          <label
-            className="login-label before:content[' '] after:content[' ']">Email Address
-          </label>
+            placeholder=" "
+          />
+          <label className="login-label before:content[' '] after:content[' ']">Email Address</label>
         </div>
         <div className="login-field">
           <input
@@ -29,15 +35,12 @@ export default function LoginInput({ login }) {
             id="password"
             className="peer login-input"
             value={password}
-            onChange={setPassword}
-            placeholder=" " />
-          <label
-            className="login-label before:content[' '] after:content[' ']">Password
-          </label>
+            onChange={setPassword} 
+            placeholder=" "
+          />
+          <label className="login-label before:content[' '] after:content[' ']">Password</label>
         </div>
-        <button type="submit" className="login-button" onClick={() => login({ email, password })}>
-          Sign In
-        </button>
+        <button type="submit" className="login-button" onClick={() => login({ email, password })}>Sign In</button>
       </div>
     </form>
   );
